c995c45e589bb7522f294f60f36d70349bb9450d
firmafadi <37545493+firmafadi@users.noreply.github.com>
2022-11-15 18:30:16 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
         return [
             'app'    => config('app.name'),
             'server' => gethostname(),
-            'description' => 'Application is Running -- on q',
+            'description' => 'Application is Running -- on q1',
             'error' => false
         ];
     }
